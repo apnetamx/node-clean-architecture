8b3a6f492e559ed70c356e785283210bd7e962eb
APS96 <andres.posada@neta.mx>
2021-10-02 22:42:46 -0500

    final changes arqui

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -1,22 +1,22 @@
-import { UserInputModel, Credential } from "../../application/domain";
-import { RepositoryMongoDB } from "../../repository";
+import { UserInputModel, Credential } from "application/domain";
+import { dbs,entities } from "../../data";
 import { RegisterUserUseCase } from "../../application/services/auth/register";
 import { Token } from "../../token";
 import { AuthenticateUserUseCase } from "../../application/services/auth/login";
 
 export class UserController {
     async registerUser(user: UserInputModel): Promise<string> {
-        const repo = RepositoryMongoDB.getInstance();
+        dbs.Repositories.getInstances();
         const tokenGenerator = new Token();
-        const useCase = new RegisterUserUseCase(repo, tokenGenerator);
+        const useCase = new RegisterUserUseCase(dbs.Repositories.mongoDB, tokenGenerator);
         const data = useCase.execute(user);
         return data;
     }
 
     async authenticateUser(details: Credential): Promise<string> {
-        const repo = RepositoryMongoDB.getInstance();
+        dbs.Repositories.getInstances();
         const tokenGenerator = new Token();
-        const useCase = new AuthenticateUserUseCase(repo, tokenGenerator);
+        const useCase = new AuthenticateUserUseCase(dbs.Repositories.mongoDB, tokenGenerator);
         const data = useCase.execute(details);
         return data;
     }
