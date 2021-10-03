3b2fd0143eb9d7200f92ed75b9162496eed5324f
APS96 <andres.posada@neta.mx>
2021-10-02 21:05:17 -0500

    Changes new Arq

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -1,29 +1,21 @@
 import { Express } from 'express';
-import { SprintRoute } from './sprint';
-import { BoardsRoute } from './boards/boards';
-import { RegisterRoute } from './register/register';
-import { LoginRoute } from './login/login';
+import { RegisterRoute } from './auth/register';
+import { LoginRoute } from './auth/login';
 
 export class Routes {
     private server: Express;
-    private sprintRoute: SprintRoute;
-    private boardsRoute: BoardsRoute;
+    private baseUrl: string = '/api/v1/';
     private registerRoute: RegisterRoute;
     private loginRoute: LoginRoute;
-    private baseUrl: string = '/api/v1/';
 
     constructor(server: Express) {
         this.server = server;
-        this.sprintRoute = new SprintRoute(server);
-        this.boardsRoute = new BoardsRoute(server);
         this.registerRoute = new RegisterRoute(server);
         this.loginRoute = new LoginRoute(server);
         this.configureApiEndPoints(this.baseUrl);
     }
 
     public configureApiEndPoints(baseUrl: string) {
-      this.sprintRoute.configureEndPoints(baseUrl);
-      this.boardsRoute.configureEndPoints(baseUrl);
       this.registerRoute.configureEndPoints(baseUrl);
       this.loginRoute.configureEndPoints(baseUrl);
     }
